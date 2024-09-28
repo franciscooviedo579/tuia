49418d096145a8288ea7efa9d9b57f469dcb5d8c
Agustin Larrubia <agustinlarrubia@gmail.com>
2024-09-28 18:20:54 +0000

    Se agrego ejercicio 2

--- programacion_II/practica_1.py
+++ programacion_II/practica_1.py
@@ -28,12 +28,42 @@ class Rectangle:
     return '(' + str(self.width) + ',' + str(self.height) + ',' + str(self.corner) + ')'
   
   def __eq__(self, other) -> bool:
+    if not isinstance(other, Point):
+      return NotImplemented
     return ( 
       self.width == other.width and
       self.height == other.height and
       self.corner == other.corner
     )
-  
+
+
+# 2.
+import copy
+
+
+def mover_rectangulo_modificadora(rect: Rectangle, dx: float, dy: float) -> None:
+  """
+  Funcion modificadora mover rectangulo
+  """
+  rect.corner.x = rect.corner.x + dx
+  rect.corner.y = rect.corner.y + dy
+
+def mover_rectangulo_pura(rect: Rectangle, dx: float, dy: float) -> Rectangle:
+  """
+  Funcion pura mover rectangulo
+  """
+  newRect = copy.deepcopy(rect)
+
+  newRect.corner.x = newRect.corner.x + dx
+  newRect.corner.y = newRect.corner.y + dy
+
+  return newRect
+
 punto = Point(5,5)
 rectangulo = Rectangle(10,20,punto)
 rectangulo2 = Rectangle(12,20,punto)
+
+#mover_rectangulo_modificadora(rectangulo,5,5)
+
+print(rectangulo.__str__())
+print(mover_rectangulo_pura(rectangulo,5,5).__str__())
