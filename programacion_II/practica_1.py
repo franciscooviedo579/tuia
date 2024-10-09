ca75dc1f65cf8a6e90d6b999cfc806e76c605149
Agustin Larrubia <agustinlarrubia@gmail.com>
2024-10-09 02:34:29 +0000

    Se agregaron ejercicios 3, 4 y 5

--- programacion_II/practica_1.py
+++ programacion_II/practica_1.py
@@ -47,10 +47,10 @@ class Rectangle:
 
 # 2.
 
-#  Defina una función llamada mover_rectángulo que tome un rectángulo y dos parámetros dx y
-# dy. Esta función debería cambiar de posición el rectángulo sumando dx a la coordenada x de la
-# esquina superior izquierda y del mismo modo sumar dy a la coordenada y de la esquina superior
-# izquierda. Haga las dos versiones, una función pura y una función modificadora.
+"""  Defina una función llamada mover_rectángulo que tome un rectángulo y dos parámetros dx y
+ dy. Esta función debería cambiar de posición el rectángulo sumando dx a la coordenada x de la
+ esquina superior izquierda y del mismo modo sumar dy a la coordenada y de la esquina superior
+ izquierda. Haga las dos versiones, una función pura y una función modificadora. """
 
 import copy
 
@@ -117,5 +117,117 @@ punto2 = Point(4,5)
 
 """ d = punto1.distancia(punto2)
 print(f"La distancia entre los puntos {punto1} y {punto2} es {d}") """
+
+
+# Ejercicio 3
+
+"""   
+ Defina la clase Automovil que contenga (al menos) los siguientes atributos:
+  • patente (string)
+  • marca (string)
+  • kilometros_recorridos (float)
+  • litros_nafta (float)
+  La clase deberá proveer un constructor que permita inicializar los atributos, siendo obligatorios patente
+y marca. kilometros_recorridos y litros_nafta, se pueden especificar o no. Si no se especifican,
+se inicializarán por defecto en 0.
+La clase tendrá además un método llamado avanzar() que recibirá como argumento el número de
+kilómetros a conducir y sumará los kilómetros recorridos al valor del atributo kilometros_recorridos.
+El método también restará al valor de litros_nafta la cantidad consumida (se calcula el consumo de
+gasolina como 8.8 litros por cada 100 kms recorridos).
+La clase también contendrá otro método llamado cargar_nafta() que recibirá como argumento los
+litros introducidos que deberán sumarse a la variable litros_nafta.
+Por último, será necesario controlar que el método avanzar nunca obtendrá un número negativo en la
+gasolina. En dicho caso, deberá mostrar el siguiente mensaje: "Es necesario cargar nafta para
+recorrer la cantidad indicada de kilómetros". 
+"""
+
+class Automovil:
+
+  def __init__(self, patente: str, marca: str, consumoPor100KM: float, kilometros_recorridos: float = 0, litros_nafta: float = 0) -> None:
+    self.patente = patente
+    self.marca = marca
+    self.kilometros_recorridos = kilometros_recorridos
+    self.litros_nafta = litros_nafta
+    self.consumoPor100KM = consumoPor100KM
+
+  def avanzar(self, kmConducir: float) -> None:
+    NAFTA_NECESARIA = (kmConducir * self.consumoPor100KM) / 100
+
+    if(self.litros_nafta < NAFTA_NECESARIA):
+      print("Es necesario cargar nafta para recorrer la cantidad indicada de kilómetros")
+    else:
+      self.kilometros_recorridos += kmConducir
+      self.litros_nafta -= (kmConducir * self.consumoPor100KM) / 100
+
+  def cargar_nafta(self, litros: float) -> None:
+    self.litros_nafta += litros
+
+
+""" Renault = Automovil("AA123BB", "Renault", 8.8, 100000, 45)
+
+Renault.avanzar(50)
+print(Renault.kilometros_recorridos)
+print(Renault.litros_nafta)
+
+Renault.cargar_nafta(23)
+
+Renault.avanzar(50)
+print(Renault.kilometros_recorridos)
+print(Renault.litros_nafta) """
+
+
+# Ejercicio 4 y 5
+class Robot:
+  def __init__(self):
+    self.x = 0
+    self.y = 0
+    self.historial = ""
+
+  def mueve(self, movimientos: str) -> None:
+
+    for movimiento in movimientos:
+      match movimiento:
+        case 'A' | 'a':
+          self.y += 1
+          self.historial += movimiento
+        case 'R' | 'r':
+          self.y -= 1
+          self.historial += movimiento
+        case 'I' | 'i':
+          self.x -= 1
+          self.historial += movimiento
+        case 'D' | 'd':
+          self.x += 1
+          self.historial += movimiento
+        case _:
+          print(f"{movimiento} Movimiento no valido")
+  
+  def posicion_actual(self) -> str:
+    return f"El robot se encuentra en ({self.x, self.y})"
+  
+  def obtener_historico_de_movimiento(self) -> str:
+    return self.historial
+  
+  def como_volver(self) -> str:
+    EJE_X = self.x * 'I' if self.x > 0 else abs(self.x) * 'D'
+    EJE_Y = self.y * 'R' if self.y > 0 else abs(self.y) * 'A'
+
+    return f"Necesita {len(EJE_X)} movimiento/s {EJE_X} en el Eje X y {len(EJE_Y)} movimiento/s {EJE_Y} en el Eje Y"
+
+
+mi_robot = Robot()
+orden = input("Introduce la orden: ")
+
+while orden != 'fin':
+  mi_robot.mueve(orden)
+  print(mi_robot.posicion_actual())
+  orden = input("Ingrese la orden, 'fin' para salir: ")
+
+print(mi_robot.obtener_historico_de_movimiento())
+print(mi_robot.como_volver())
+
+
+
+
 
 
