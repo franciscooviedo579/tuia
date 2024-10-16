33d314e0d0e8d958d834ddd27333d39eee72c09a
Agustin Larrubia <agustinlarrubia@gmail.com>
2024-10-16 21:15:30 +0000

    Se agrego ejercicio 6, 7 y 8

--- programacion_II/practica_1.py
+++ programacion_II/practica_1.py
@@ -215,7 +215,7 @@ class Robot:
     return f"Necesita {len(EJE_X)} movimiento/s {EJE_X} en el Eje X y {len(EJE_Y)} movimiento/s {EJE_Y} en el Eje Y"
 
 
-mi_robot = Robot()
+""" mi_robot = Robot()
 orden = input("Introduce la orden: ")
 
 while orden != 'fin':
@@ -224,10 +224,195 @@ while orden != 'fin':
   orden = input("Ingrese la orden, 'fin' para salir: ")
 
 print(mi_robot.obtener_historico_de_movimiento())
-print(mi_robot.como_volver())
+print(mi_robot.como_volver()) """
+
+# Ejercicio 6
+
+""" 
+Crear las clases Materia y Carrera.
+Cada materia tiene: - Un código de materia. - Un nombre de la materia. - Una cantidad de créditos
+que aporta.
+Una carrera puede pensarse como una lista de materias.
+"""
+
+class Materia:
+  def __init__(self, codigoMat: str, nombreMat: str, creditosMat: int) -> None:
+    self.codigoMat = codigoMat
+    self.nombreMat = nombreMat
+    self.creditosMat = creditosMat
+
+class Carrera:
+  """
+  Lista de materias
+  """
+  def __init__(self, materias: list[Materia]) -> None:
+    self.materias = materias
+    self.creditos = 0
+    self.promedio = None
+    self.materiasAprobadas = []
+
+  def __str__(self) -> str:
+    return f"Créditos: {self.creditos} -- Promedio: {self.calc_promedio()} -- Materias aprobadas: {self.print_materias_aprobadas()}"
+  
+  def add_creditos(self, materia: Materia) -> None :
+    self.creditos += materia.creditosMat
+
+  def calc_promedio(self) -> float | str:
+    if(len(self.materiasAprobadas) == 0): return "N/A"
+    totalAprobadas = len(self.materiasAprobadas)
+    totalNotas = 0
+    for materiaAprobada in self.materiasAprobadas:
+      nota = materiaAprobada["nota"]
+      totalNotas += nota
+
+    return totalNotas / totalAprobadas
+
+    # for materia in materiasAprobadas:
+
+  def aprobar(self, codigoMat: str, nota: float) -> None:
+    
+    for materia in self.materias:
+      if(materia.codigoMat == codigoMat):
+        self.materiasAprobadas.append({"materia": materia, "nota": nota})
+        self.creditos += materia.creditosMat
+        return
+    
+    print(f"Error la materia {codigoMat} no es parte del plan de estudios.")
+  
+  def print_materias_aprobadas(self) -> str:
+
+    materiasAprob = ""
+    if(len(self.materiasAprobadas) == 0): return materiasAprob
+
+    for materia in self.materiasAprobadas:
+      nota = materia["nota"]
+      infoMateria = materia["materia"]
+      materiasAprob += f"{infoMateria.codigoMat} {infoMateria.nombreMat} ({nota}) "
+    
+    return materiasAprob
+
+# --- Comportamiento esperado ---  
+""" analisis2 = Materia("61.03", "Análisis 2", 8)
+fisica2 = Materia("62.01", "Física 2", 8)
+algo1 = Materia("75.40", "Algoritmos 1", 6)
+c = Carrera([analisis2, fisica2, algo1])
+print(c) """
+#Créditos: 0 -- Promedio: N/A -- Materias aprobadas:
+""" c.aprobar("95.14", 7) """
+#Error: La materia 75.14 no es parte del plan de estudios
+""" c.aprobar("75.40", 10)
+c.aprobar("62.01", 7)
+print(c) """
+#Créditos: 14 -- Promedio: 8.5 -- Materias aprobadas: 75.40 Algoritmos 1 (10) 62.01 Física 2 (7)
 
 
+# Ejercicio 7
+
+""" Encontrar los errores en el siguiente código y proponer soluciones: """
+""" 
+class Cosa:
+  def __init__(self, valor):
+    self.valor = valor
+
+class Coleccion:
+  def __init__(self):
+    self.coleccion = []
+  def agregar_cosa(cosa: Cosa):
+    coleccion.append(cosa) # No se paso self como parametro y a coleccion le falta self.coleccion
+
+cosa = Cosa() # No se pasa parametro para inicializar valor
+coleccion = Coleccion()
+coleccion.agregar_cosa(cosa) """
+  
+class Cosa:
+  def __init__(self, valor):
+    self.valor = valor
+
+class Coleccion:
+  def __init__(self):
+    self.coleccion = []
+  def agregar_cosa(self, cosa: Cosa):
+    self.coleccion.append(cosa)
+
+""" cosa = Cosa(123)
+coleccion = Coleccion()
+coleccion.agregar_cosa(cosa)  
+
+print(coleccion.coleccion) """
+
+# Ejercicio 8
+
+""" 
+  Programe un conjunto de seis clases que modele esta taxonomía utilizando clases. Luego, agregue un
+método speak a cada clase imprimiendo un mensaje apropiado a cada clase (por ejemplo, una instancia
+de animal podría imprimir "Soy un animal").
+Luego, agregue un método talk a la clase Animal, que simplemente delegue el funcionamiento en
+speak. ¿Qué ocurre al llamar a talk en una subclase? ¿Qué ocurre si borramos el método speak de la
+clase Hacker?
+"""
+
+class Animal:
+  def __init__(self, nombre: str) -> None:
+    self.nombre = nombre
+  
+  def talk(self) -> None:
+    self.speak()
+
+  def speak(self) -> None:
+    print("Soy un animal")
+    
+class Mamiferos(Animal):
+  def __init__(self, nombre: str) -> None:
+    super().__init__(nombre)
+  
+  def speak(self) -> None:
+    print("Soy un mamifero")
+
+  
+class Felinos(Mamiferos):
+  def __init__(self, nombre: str) -> None:
+    super().__init__(nombre)
+  
+  def speak(self) -> None:
+    print("Soy un felino")
+
+  
+class Canidos(Mamiferos):
+  def __init__(self, nombre: str) -> None:
+    super().__init__(nombre)
+  
+  def speak(self) -> None:
+    print("Soy un canido")
+
+  
+class Primates(Mamiferos):
+  def __init__(self, nombre: str) -> None:
+    super().__init__(nombre)
+  
+  def speak(self) -> None:
+    print("Soy un primate")
+
+  
+class Hacker(Primates):
+  def __init__(self, nombre: str) -> None:
+    super().__init__(nombre)
+  
+  def speak(self) -> None:
+    print("Soy un Kaker")
 
 
+animal = Animal("Animal")
+mamifero = Mamiferos("Mamifero")
+felino = Felinos("Felino")
+canido = Canidos("Canido")
+primate = Primates("Primate")
+hacker = Hacker("Hacker")
 
+animal.speak()
+mamifero.speak()
+felino.speak()
+canido.speak()
+primate.speak()
+hacker.speak()
 
+hacker.talk()
